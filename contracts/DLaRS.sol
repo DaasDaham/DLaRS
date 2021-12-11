ec85965d38f3cda720dfc9898de7f615c431a990
Saad Ahmad <saad18409@iiitd.ac.in>
2021-12-12 01:43:42 +0530

    Payment Updated

--- contracts/DLaRS.sol
+++ contracts/DLaRS.sol
@@ -1,6 +1,165 @@
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity 0.8.1;
+
+/**
+ * @dev Wrappers over Solidity's arithmetic operations with added overflow
+ * checks.
+ *
+ * Arithmetic operations in Solidity wrap on overflow. This can easily result
+ * in bugs, because programmers usually assume that an overflow raises an
+ * error, which is the standard behavior in high level programming languages.
+ * `SafeMath` restores this intuition by reverting the transaction when an
+ * operation overflows.
+ *
+ * Using this library instead of the unchecked operations eliminates an entire
+ * class of bugs, so it's recommended to use it always.
+ */
+library SafeMath {
+    /**
+     * @dev Returns the addition of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `+` operator.
+     *
+     * Requirements:
+     * - Addition cannot overflow.
+     */
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting on
+     * overflow (when the result is negative).
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     * - Subtraction cannot overflow.
+     */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return sub(a, b, "SafeMath: subtraction overflow");
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting with custom message on
+     * overflow (when the result is negative).
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     * - Subtraction cannot overflow.
+     */
+    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b <= a, errorMessage);
+        uint256 c = a - b;
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the multiplication of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `*` operator.
+     *
+     * Requirements:
+     * - Multiplication cannot overflow.
+     */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers. Reverts on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     * - The divisor cannot be zero.
+     */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return div(a, b, "SafeMath: division by zero");
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers. Reverts with custom message on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     * - The divisor cannot be zero.
+     */
+    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        // Solidity only automatically asserts when dividing by 0
+        require(b > 0, errorMessage);
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * Reverts when dividing by zero.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     * - The divisor cannot be zero.
+     */
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        return mod(a, b, "SafeMath: modulo by zero");
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * Reverts with custom message when dividing by zero.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     * - The divisor cannot be zero.
+     */
+    function mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b != 0, errorMessage);
+        return a % b;
+    }
+    
+    function ceil(uint256 a, uint256 m) internal pure returns (uint256) {
+        uint256 c = add(a,m);
+        uint256 d = sub(c,1);
+        return mul(div(d,m),m);
+    }
+}
+
+
 
 contract DLars{
+
+    using SafeMath for uint;
 
     //Structure for all the land Details
     struct landDetails {
@@ -65,22 +224,34 @@ contract DLars{
         Lands[landId].status = LandStatus.underBidding;
     }
 
-    function placeBid(address payable _receiver, uint256 amount) public payable {
-        //PaymentReceived[_landId] = true;
-        uint256 amntToRed = msg.value - 5;
-        _receiver.transfer(amntToRed);
-    }
+    // function placeBid(address payable _receiver1, address payable _receiver2, uint256 amount) public payable {
+    //     //PaymentReceived[_landId] = true;
+    //     //payable(Lands[landId].currentOwner).transfer(currentBid-Auction[landId].highestBid);
+    //     //uint amntToRed = msg.value-5000000000000000000;
+    //     //_receiver1.transfer(amntToRed);
+    //     //_receiver2.transfer(4000000000000000000);
+    // }
+
+    // function receiveBid() public payable{
+    //     require(msg.value>0);
+    // }
+
+    // function withdraw(uint amnt) public{
+    //     msg.sender.transfer(amnt);
+    // }
     
-    /*function placeBid(uint landId, uint currentBid) public payable{
+    function placeBid(uint landId) public payable{
         //TO BE DONE
+        uint currentBid = msg.value;
         require(msg.sender!=Lands[landId].currentOwner);
         require(Auction[landId].highestBid<currentBid);
         require(Lands[landId].askingPrice<currentBid);
         require(Lands[landId].status==LandStatus.underBidding);
-        //if (Auction[landId].highestBid>0){
-        //    Auction[landId].highestBidder.transfer(Auction[landId].highestBid);
-        //}
-        payable(Lands[landId].currentOwner).transfer(currentBid-Auction[landId].highestBid);
+        if (Auction[landId].highestBid>0){
+            Auction[landId].highestBidder.transfer(Auction[landId].highestBid);
+        }
+        uint toTransfer = currentBid-Auction[landId].highestBid;
+        payable(Lands[landId].currentOwner).transfer(toTransfer);
         Auction[landId].highestBidder = payable(msg.sender);
         Auction[landId].highestBid = currentBid;
         Auction[landId].highestBidTimestamp = block.timestamp;
@@ -89,7 +260,7 @@ contract DLars{
         // check current bid > last bid, pay amount, update highest bid, 
         // check if there was a last bidder, if yes revert amount of last bidder    
 
-    }*/
+    }
 
     //This function allows the current owner to view the current highest bid
     function viewHighestBid(uint landId) public onlyOwner(landId) returns(uint){
