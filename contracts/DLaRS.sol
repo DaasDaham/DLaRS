57d7171e1a270479e4c2437926a40c88358a2da3
aman18327 <aman18327@iiitd.ac.in>
2021-12-12 16:01:57 +0530

    Added viewLandDetails and reorganised some variables

--- contracts/DLaRS.sol
+++ contracts/DLaRS.sol
@@ -1,47 +1,62 @@
 pragma solidity 0.8.1;
 // SPDX-License-Identifier: UNLICENSED
 
-
 contract DLars{
 
     //Structure for all the land Details
     struct landDetails {
         string city;
         string country;
-        string landAdress;
+        string landAddress;
         string pincode;
         address payable currentOwner;
-        uint askingPrice;
         LandStatus status;
-        bool isRegistered;
+        bool isRegistered;                  // isRegistered is true if a land is already present in the mapping otherwise false
     }
+
+    // enum named "LandStatus" denoting the stage of Auction the land is in
     enum LandStatus {underBidding, registered}
+
+    // mapping storing the land details
     mapping(uint => landDetails) Lands;
+
+    // address of the manager of contract
     address manager;
+
+    // modifier ensuring only manager access
     modifier onlyManager{ 
-        //Ensuring only manager can add new Land
         require(msg.sender == manager);
         _;
     }
+
+    // modifier ensuring only current land owner access
     modifier onlyOwner(uint landId){
         //Ensuring some features can only be accessed by current owner
         require(msg.sender == Lands[landId].currentOwner);
         _;
     }
-    mapping(uint => auction) Auction;
+
+    // structure for all the auction details
     struct auction {
         //Auction are identified using their landId
 	    address payable highestBidder;
 	    uint highestBid;
+        uint askingPrice;
+        uint minBidInterval;
         uint highestBidTimestamp;
     }   
-    constructor() public{
+
+    // mapping storing the auction details for a land
+    mapping(uint => auction) Auction;
+
+    // constructor giving manager access to the deployer of the contract
+    constructor() {
         manager = msg.sender;
     }
     
     //This function creates unique landId using its properties
-    function computeIdLand(string memory landAddress, string memory city,string memory pincode) private pure returns(uint){    
-        return uint(keccak256(abi.encodePacked(landAddress, city, pincode)))%1000000000;
+    function computeIdLand(string memory landAddress, string memory city,string memory country, string memory pincode) private pure returns(uint){    
+        return uint(keccak256(abi.encodePacked(landAddress, city, country, pincode)))%1000000000;
     }
     //This function registers land on DLaRS and can only be called by manager
     function registerLand(string memory landAddress, string memory city, string memory country, 
@@ -49,8 +64,9 @@ contract DLars{
         // Add a new entry in Land mapping with a new id
         // Status of the land will be registered
 
-        uint landID = computeIdLand(landAddress, city, pincode);
+        uint landID = computeIdLand(landAddress, city, country, pincode);
         require(!Lands[landID].isRegistered);
+        Lands[landID].landAddress = landAddress;
         Lands[landID].city = city;
         Lands[landID].country = country;
         Lands[landID].pincode = pincode;
@@ -59,14 +75,29 @@ contract DLars{
         Lands[landID].isRegistered = true;
         return landID;
     }
+
+    //This function allows the current owner to view the current highest bid
+    function viewLandDetails(uint landId) public view returns(string memory, string memory, string memory, string memory, LandStatus){
+        // accessed by anyone
+        // returns all the details of a land
+        return (Lands[landId].city, 
+                Lands[landId].country,
+                Lands[landId].landAddress,
+                Lands[landId].pincode,
+                Lands[landId].status
+                );
+    }
+    
     //This function puts up the land for auction, can only be called by the current owner    
-    function putUpForAuction(uint landId, uint askingPrice) public onlyOwner(landId){
+    function putUpForAuction(uint landId, uint askingPrice, uint minBidInterval) public onlyOwner(landId){
         // Changes land status to underBidding and land is up for auction.
         // Add entry in auction
         require(Lands[landId].status == LandStatus.registered);
+        require(minBidInterval > 0);
         Auction[landId].highestBidder = payable(address(0));
         Auction[landId].highestBid = 0;
-        Lands[landId].askingPrice = askingPrice;
+        Auction[landId].askingPrice = askingPrice;
+        Auction[landId].minBidInterval = minBidInterval;
         Auction[landId].highestBidTimestamp = block.timestamp;
         Lands[landId].status = LandStatus.underBidding;
     }
@@ -77,38 +108,30 @@ contract DLars{
         require(Lands[landId].status == LandStatus.underBidding);
         require(Auction[landId].highestBid == 0);
         Lands[landId].status = LandStatus.registered;
-        Lands[landId].askingPrice = 0;
+        Auction[landId].askingPrice = 0;
+        Auction[landId].minBidInterval = 0;
     }
 
-    function updateAskingPrice(uint landId, uint newAskingPrice) public onlyOwner(landId){
+    function updateAskingPrice(uint landId, uint newAskingPrice, uint newMinBidInterval) public onlyOwner(landId){
         // changes asking price in auction if there has not been a bid yet
         // only accessible by current owner of land
         require(Lands[landId].status == LandStatus.underBidding);
         require(Auction[landId].highestBid == 0);
-        Lands[landId].askingPrice = newAskingPrice;
+        require(newMinBidInterval > 0);
+        Auction[landId].askingPrice = newAskingPrice;
+        Auction[landId].minBidInterval = newMinBidInterval;
     }
-    // function placeBid(address payable _receiver1, address payable _receiver2, uint256 amount) public payable {
-    //     //PaymentReceived[_landId] = true;
-    //     //payable(Lands[landId].currentOwner).transfer(currentBid-Auction[landId].highestBid);
-    //     //uint amntToRed = msg.value-5000000000000000000;
-    //     //_receiver1.transfer(amntToRed);
-    //     //_receiver2.transfer(4000000000000000000);
-    // }
-
-    // function receiveBid() public payable{
-    //     require(msg.value>0);
-    // }
-
-    // function withdraw(uint amnt) public{
-    //     msg.sender.transfer(amnt);
-    // }
     
     function placeBid(uint landId) public payable{
-        //TO BE DONE
+        // params: pass auction Id and 
+        // accessible by anyone except the seller
+        // check current bid > last bid, pay amount, update highest bid, 
+        // check if there was a last bidder, if yes revert amount of last bidder 
+       
         uint currentBid = msg.value;
         require(msg.sender!=Lands[landId].currentOwner);
-        require(Auction[landId].highestBid<currentBid);
-        require(Lands[landId].askingPrice<=currentBid);
+        require(Auction[landId].highestBid + Auction[landId].minBidInterval < currentBid);
+        require(Auction[landId].askingPrice<=currentBid);
         require(Lands[landId].status==LandStatus.underBidding);
         if (Auction[landId].highestBid>0){
             Auction[landId].highestBidder.transfer(Auction[landId].highestBid);
@@ -118,19 +141,17 @@ contract DLars{
         Auction[landId].highestBidder = payable(msg.sender);
         Auction[landId].highestBid = currentBid;
         Auction[landId].highestBidTimestamp = block.timestamp;
-        // accessible by bidders only
-        // params: pass auction Id and 
-        // check current bid > last bid, pay amount, update highest bid, 
-        // check if there was a last bidder, if yes revert amount of last bidder    
 
     }
 
-    //This function allows the current owner to view the current highest bid
-    function viewHighestBid(uint landId) public view returns(uint){
+    //This function allows the current owner to view the auction details including highest bid, initial asking price, highest bid's timestamp
+    function viewAuctionDetails(uint landId) public view returns(uint,uint,uint){
         // accessed by seller
         // returns highest bid value for the current land
         require(Lands[landId].status == LandStatus.underBidding);
-        return Auction[landId].highestBid;
+        return (Auction[landId].highestBid,
+                Auction[landId].askingPrice,
+                Auction[landId].highestBidTimestamp);
     }
 
     function terminateAuction(uint landId) public {
@@ -151,7 +172,7 @@ contract DLars{
         // (auction will take care of guarantee that money will be transferred)
         // Will internally call the transfer ownership function        
         require(Lands[landId].status == LandStatus.underBidding);
-        require(Auction[landId].highestBid >= Lands[landId].askingPrice);
+        require(Auction[landId].highestBid >= Auction[landId].askingPrice);
         transferOwnership(landId, Auction[landId].highestBidder);  
         Lands[landId].status = LandStatus.registered;     
     }
